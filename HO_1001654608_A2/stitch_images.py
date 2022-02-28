ba4e790011f8f13fa56311778fff260a475c69ec
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2022-02-27 22:48:47 -0600

    Assignment 2 Skeleton

---
+++ HO_1001654608_A2/stitch_images.py
@@ -0,0 +1,13 @@
+#Hoang Ho - 1001654608
+#CSE 4310-001 Fundamentals of Computer Vision
+#Assignment 2 - SIFT and RANSAC
+#Due March 4, 2022
+
+#1 Keypoint Detection
+#2 Keypoint Matching
+#2.1 Plot Keypoint Matches
+#3 Image Stitching
+#3.1 Estimate Affine MAtrix
+#3.2 Estimate Projective Matrix
+#3.3 RANSAC
+#3.4 Testing
