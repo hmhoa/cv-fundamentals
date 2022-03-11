d424d4caa45a4a7f6ac0e61161b3171ab05bbcfb
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2022-03-10 19:05:50 -0600

    Implemented keypoint matching

--- HO_1001654608_A2/stitch_images.py
+++ HO_1001654608_A2/stitch_images.py
@@ -1,13 +1,15 @@
-#Hoang Ho - 1001654608
-#CSE 4310-001 Fundamentals of Computer Vision
-#Assignment 2 - SIFT and RANSAC
-#Due March 11, 2022 by 11:59 PM
+# Hoang Ho - 1001654608
+# CSE 4310-001 Fundamentals of Computer Vision
+# Assignment 2 - SIFT and RANSAC
+# Due March 11, 2022 by 11:59 PM
 
-#ensure latest version of scikit-image with SIFT installed
-#conda  search scikit-image -c conda-forge
-#conda install scikit-image=0.19.2 -c conda-forge
+# ensure latest version of scikit-image with SIFT installed
+# conda  search scikit-image -c conda-forge
+# conda install scikit-image=0.19.2 -c conda-forge
 
 #do imports
+from math import sqrt
+from sys import float_info
 import numpy as np
 import PIL.Image as Image #enable reading images
 import matplotlib.pyplot as plt
@@ -20,53 +22,84 @@ from skimage import measure
 
 
 
-#2 Keypoint Matching
-#returns a list of indices of matching feature pairs
-#pair (i,j) in the list indicates a match between the feature at index i in the src image with the feature at index j in the second image
+# 2 Keypoint Matching
+# keypoint1, keypoint2 - list of keypoint coordinates from dst and src images respectively
+# keypoints represented as a (N,2) array. Keypoint coordinates as (row, col)
+# returns a list of indices of matching feature pairs where a pair (i,j) in the list indicates a match between the feature at index i in the src image with the feature at index j in the second image
 def match(keypoints1, keypoints2):
-    return
-    #lowest match w L2 distance
+    #list of tuple pairs (i,j) of matching keypoints
+    matches = []
+    
+    # choose the keypoint array with the smallest length to determine for loop iteration amount
+    # keypoints1_size = np.size(keypoints1, axis=0)
+    # keypoints2_size = np.size(keypoints2, axis=0)
+    # length = min(keypoints1_size, keypoints2_size)
+
+    # using brute force and calculating the lowest matches using euclidean distance
+    # outer loop iterates through each keypoint in keypoints1
+    # kp = keypoint, so kp1 is a keypoint from keypoints 1 and kp2 is a keypoint from keypoints 2
+    smallest_distance = float_info.max
+    i, j = 0
+
+    for kp1 in keypoints1:
+        # iterate through each keypoint in keypoints2 and find the smallest euclidean distance of kp1 and kp2
+        for kp2 in keypoints2:
+            distance = sqrt(((kp2[0]-kp1[0])**2)+((kp2[1]-kp1[1])**2)) #calculate euclidean distance of the current keypoint pair
+            if distance < smallest_distance:
+                smallest_distance = distance
+                #update current indices of matching feature pair
+                matching_pair = (i,j)
+            j += 1
+        #append this matching_pair to the matches list
+        matches.append(matching_pair)
+
+        #reset indices counter
+        j = 0
+
+    return matches
+    
 
 
-#2.1 Plot Keypoint Matches
-#calculate the size of the images that you're given (both will be same resolution), so double it
+# 2.1 Plot Keypoint Matches
+# plotting function that combines two input images of the same size side-by-side and plots the keypoitns detected in each  image. Plot the lines  between the keypoints shwoing which ones have been matched together
+# calculate the size of the images that you're given (both will be same resolution), so double it
 
 
-#3 Image Stitching
-#3.1 Estimate Affine MAtrix
-#3.2 Estimate Projective Matrix
-#3.3 RANSAC
-#follow basic approach of randomly selecting poitns fitting the  model that are goon  points, taking entire dataset and determining if it was a good point dedpending on number of inliers
+# 3 Image Stitching
+# 3.1 Estimate Affine MAtrix
+# 3.2 Estimate Projective Matrix
+# 3.3 RANSAC
+# follow basic approach of randomly selecting poitns fitting the  model that are goon  points, taking entire dataset and determining if it was a good point dedpending on number of inliers
 #destination image is the one not being warped
 
-#3.4 Testing
+# 3.4 Testing
 def main():
-    #set up images
+    # set up images
     
     dst_img_rgb = np.asarray(Image.open('a2_images/Rainier1.png'))
     src_img_rgb = np.asarray(Image.open('a2_images/Rainier2.png'))
 
-    #checking if theres a 4th channel - alpha and turning back into rgb
-    if dst_img_rgb.shape[2] = 4:
+    # checking if theres a 4th channel - alpha and turning back into rgb
+    if dst_img_rgb.shape[2] == 4:
         dst_img_rgb = rgba2rgb(dst_img_rgb)
-    if src_img_rgb.shape[2] = 4:
+    if src_img_rgb.shape[2] == 4:
         src_img_rgb = rgba2rgb(src_img_rgb)
 
-    #grayscale the images
+    # grayscale the images
     dst_img = rgb2gray(dst_img_rgb)
     src_img = rgb2gray(src_img_rgb)
 
 
-    #plotting the set up images (nothing changed so far)
+    # plotting the set up images (nothing changed so far)
     fig = plt.figure(figsize=(8, 4))
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
     ax1.imshow(dst_img, cmap='gray')
     ax2.imshow(src_img, cmap='gray')
 
-    #1 Keypoint Detection - extract features that describe the object in the image
-    #descriptor - describe/define features despite orientation, size, position, etc.
-    #can do harris corner detection
+    # 1 Keypoint Detection - extract features that describe the object in the image
+    # descriptor - describe/define features despite orientation, size, position, etc.
+    # can do harris corner detection
     dst_detector = SIFT()
     dst_detector.detect_and_extract(dst_img) #detects keypoints using FAST corner detection and extracts rBRIEF descriptors
     dst_keypoints = dst_detector.keypoints #represented as a (N,2) array. Keypoint coordinates as (row, col)
@@ -77,7 +110,12 @@ def main():
     src_keypoints = src_detector.keypoints
     src_descriptors = src_detector.descriptors
 
-    #2 Keypoint Matching
+    # 2 Keypoint Matching
+    matches = match(dst_keypoints, src_keypoints)
+    print('Matches found (i,j):')
+    print(matches)
+
+    
 
 
 if __name__ == "__main__":
