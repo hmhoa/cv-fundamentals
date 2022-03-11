998c5475e431599b43ff62a887bf6f0086f1d262
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2022-03-10 23:01:59 -0600

    Fixed matching to match based on descriptors

--- HO_1001654608_A2/stitch_images.py
+++ HO_1001654608_A2/stitch_images.py
@@ -23,34 +23,34 @@ from skimage import measure
 
 
 # 2 Keypoint Matching
-# keypoint1, keypoint2 - list of keypoint coordinates from dst and src images respectively
+# feature1, feature2 - set of keypoint features from dst and src images respectively
 # keypoints represented as a (N,2) array. Keypoint coordinates as (row, col)
 # returns a list of indices of matching feature pairs where a pair (i,j) in the list indicates a match between the feature at index i in the src image with the feature at index j in the second image
-def match(keypoints1, keypoints2):
-    #list of tuple pairs (i,j) of matching keypoints
+def match(dst_features, src_features):
+    #list of pairs [i,j] of matching keypoints
     matches = []
-    
-    # choose the keypoint array with the smallest length to determine for loop iteration amount
-    # keypoints1_size = np.size(keypoints1, axis=0)
-    # keypoints2_size = np.size(keypoints2, axis=0)
-    # length = min(keypoints1_size, keypoints2_size)
+
+    dst_descriptors = dst_features.descriptors # represented as a (N, n_hist*n_hist*n_ori) array; vector of all values of histograms
+
+    src_descriptors = src_features.descriptors
 
     # using brute force and calculating the lowest matches using euclidean distance
     # outer loop iterates through each keypoint in keypoints1
-    # kp = keypoint, so kp1 is a keypoint from keypoints 1 and kp2 is a keypoint from keypoints 2
+    # d = descriptor, so d1 is a descriptor from dst_descriptors and d2 is a descriptor from src_descriptors
     i = 0
-    for kp1 in keypoints1:
+    for d1 in dst_descriptors:
         #reset indices counter and smallest distance for next keypoint to analyze
         j = 0
         smallest_distance = float_info.max
         
-        # iterate through each keypoint in keypoints2 and find the smallest euclidean distance of kp1 and kp2
-        for kp2 in keypoints2:
-            distance = sqrt(((kp2[0]-kp1[0])**2)+((kp2[1]-kp1[1])**2)) #calculate euclidean distance of the current keypoint pair
+        # iterate through each keypoint in keypoints2 and find the smallest euclidean distance of d1 and d2
+        for d2 in src_descriptors:
+            squared_differences = np.square((d2[:]-d1[:])) #subtracts values for each dimension and squares it
+            distance = sqrt(np.sum(squared_differences)) #calculate euclidean distance of the current descriptor pair
             if distance < smallest_distance:
                 smallest_distance = distance
                 #update current indices of matching feature pair
-                matching_pair = (i,j)
+                matching_pair = [i,j]
             j += 1
         #append this matching_pair to the matches list if it is not already in the list
         if matching_pair not in matches:
@@ -61,11 +61,41 @@ def match(keypoints1, keypoints2):
 
     return matches
     
-
-
 # 2.1 Plot Keypoint Matches
-# plotting function that combines two input images of the same size side-by-side and plots the keypoitns detected in each  image. Plot the lines  between the keypoints shwoing which ones have been matched together
+# plotting function that combines two input images of the same size side-by-side and plots the keypoint detected in each  image. Plot the lines  between the keypoints shwoing which ones have been matched together
 # calculate the size of the images that you're given (both will be same resolution), so double it
+# dst_image, src_image - input images to be combined side by side as np arrays
+# dst_kp_matches, src_kp_matches - keypoints that match with destination image and source image
+def plot_matches(dst_image, src_image, dst_kp_matches, src_kp_matches):
+    # combine the two images into one image
+    height, width, channels = dst_image.shape
+
+    total_width = width*2
+
+    combined_img = np.zeros((height, total_width,channels))
+    print(f'Combined Image Shape: {combined_img.shape}')
+    combined_img[:height, :width, :] = dst_image
+    combined_img[:height, width:, :] = src_image
+
+    # plot the points and connect them
+    num_matches = len(dst_kp_matches)
+
+    for i in range(5):
+        # retrieve the points
+        dst_point = (dst_kp_matches[i,1], dst_kp_matches[i,0])
+        src_point = (src_kp_matches[i,1], src_kp_matches[i,0])
+
+        #pass a list of two matching keypoints we want to connect to plt.plot
+        plt.plot([dst_point[0], src_point[0]+width], [dst_point[1],src_point[1]], 'r-o') # ro- specifies we want to plot red marker with a line connecting the two points
+
+    #to get rid of the warning: Clipping input data to the valid range for imshow with RGB data ([0..1] for floats or [0..255] for integers).
+    if type(combined_img) is float:
+        combined_img = combined_img/np.amax(combined_img)
+    elif type(combined_img) is int:
+        combined_img = np.array(combined_img/np.amax(combined_img)*255, np.int32)
+
+    plt.imshow(combined_img)
+    plt.show()
 
 
 # 3 Image Stitching
@@ -94,17 +124,25 @@ def main():
 
 
     # plotting the set up images (nothing changed so far)
-    fig = plt.figure(figsize=(8, 4))
-    ax1 = fig.add_subplot(121)
-    ax2 = fig.add_subplot(122)
-    ax1.imshow(dst_img, cmap='gray')
-    ax2.imshow(src_img, cmap='gray')
+    # fig, ax = plt.figure(figsize=(8, 4))
+    # ax1 = fig.add_subplot(121)
+    # ax2 = fig.add_subplot(122)
+    # ax1.imshow(dst_img, cmap='gray')
+    # ax2.imshow(src_img, cmap='gray')
+
+    fig, ax = plt.subplots(1,2) #1 row, 2 columns for side-by-side subplots
+    ax[0].set_title('Destination Image')
+    ax[0].imshow(dst_img, cmap='gray')
+    ax[1].set_title('Source Image')
+    ax[1].imshow(src_img, cmap='gray')
+
+    plt.show()
 
     # 1 Keypoint Detection - extract features that describe the object in the image
     # descriptor - describe/define features despite orientation, size, position, etc.
     # can do harris corner detection
     dst_detector = SIFT()
-    dst_detector.detect_and_extract(dst_img) #detects keypoints using FAST corner detection and extracts rBRIEF descriptors
+    dst_detector.detect_and_extract(dst_img) #detects keypoints and extracts descriptors
     dst_keypoints = dst_detector.keypoints #represented as a (N,2) array. Keypoint coordinates as (row, col)
     dst_descriptors = dst_detector.descriptors #represented as a (N, n_hist*n_hist*n_ori) array; default is n_hist = 4, n_ori = 8
     
@@ -119,12 +157,18 @@ def main():
     print(src_keypoints)
 
     # 2 Keypoint Matching
-    matches = match(dst_keypoints, src_keypoints)
+    matches = np.asarray(match(dst_detector, src_detector)) #store the results as a numpy array for slicing later
     print(f'\n{len(matches)} Matches found (i,j):')
     print(matches)
 
-    
+    # select the points in the destination image that match with the source image
+    dst_matches = dst_keypoints[matches[:,0]] # select the dst_keypoints at the indices i listed in the first column of matches
+    src_matches = src_keypoints[matches[:,1]] # select the src_keypoints at the indices j listed in the second column of matches
 
+    # 2.1 Plot Keypoint Matches
+    plot_matches(dst_img_rgb, src_img_rgb, dst_matches, src_matches)
+
+    
 
 if __name__ == "__main__":
     main()
