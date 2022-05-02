b68507dc91831748049df3f4049a2e9dd6234340
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2022-05-02 03:40:59 -0500

    Create BasicCNN.py

---
+++ HO_1001654608_A4/BasicCNN.py
@@ -0,0 +1,8 @@
+# Hoang Ho - 1001654608
+# CSE 4310-001 Fundamentals of Computer Vision
+# Assignment 4 - Convolutional Neural Networks
+# Due May 11, 2022 by 11:59 PM
+
+# references 
+
+# pick something that resembles like an Alex net or lenet where you ahve a couple of convolutions - maybe try some net pooling or change pooling size
