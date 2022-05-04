acb123b65ceadf330adf329ff2965dc00c1a4a12
hmh4608 <60151667+hmh4608@users.noreply.github.com>
2022-05-03 19:51:48 -0500

    Added strides to AllCN

--- HO_1001654608_A4/AllCN.py
+++ HO_1001654608_A4/AllCN.py
@@ -43,9 +43,9 @@ class AllCN(pl.LightningModule):
         self.features = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3),
             nn.ReLU(),
-            nn.Conv2d(8, 32, 3),
+            nn.Conv2d(8, 32, 3, strides=2),
             nn.ReLU(),
-            nn.Conv2d(32, 128, 5),
+            nn.Conv2d(32, 128, 5, strides=2),
             nn.ReLU(),
             nn.Conv2d(128, 256, 5), 
             nn.ReLU(),
